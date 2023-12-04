86521cdbc4330d2384bb9fbcc8f221fd68eb8285
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-12-04 22:27:02 +0300

    feature: added ecosystem.config.js for backend

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'mesto-backend',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build &&
+                      pm2 startOrRestart ecosystem.config.js --env production && pm2 save`,
+    },
+  },
+};
