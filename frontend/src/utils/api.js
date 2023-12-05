c1ad7b1eeeea57c0d16596cb6ae0e6628290bd57
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-12-05 15:59:47 +0300

    bugfix: api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,7 +139,7 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.mesto.skuhtalev.nomoredomainsmonster.ru/');
+  const api = new Api('https://api.mesto.skuhtalev.nomoredomainsmonster.ru');
 
   export default api;
 
